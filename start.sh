f722814f83906e6f069dbd9bf0251aef056d7289
hackerlove123 <149338972+hackerlove123@users.noreply.github.com>
2025-03-27 14:20:23 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e  # Tự động thoát nếu có lỗi
-
 # ==========================================
 # Phần 1: Chạy node ok.js trong background
 # ==========================================
